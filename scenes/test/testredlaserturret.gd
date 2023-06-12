b8bfb2b733cf818d55662f7c263b094f3aba3a78
synodriver <diguohuangjiajinweijun@gmail.com>
2023-06-12 10:20:46 +0800

    update test

--- scenes/test/testredlaserturret.gd
+++ scenes/test/testredlaserturret.gd
@@ -5,9 +5,9 @@ func _ready():
 
 func _input(event):
 	if event is InputEventMouseButton:
-			if event.button_index == MouseButton.MOUSE_BUTTON_LEFT:
-				if event.is_pressed():
-					self.get_node("RedLaserTurret").fire()
+		if event.button_index == MouseButton.MOUSE_BUTTON_LEFT:
+			if event.is_pressed():
+				self.get_node("RedLaserTurret").fire()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
